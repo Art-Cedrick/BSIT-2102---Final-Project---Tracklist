28de378613ce67f7eee3a1c1a76d98ece7e7b879
Daniel Isaaiah Cueto <113603567+DanielIsaaiahCueto@users.noreply.github.com>
2022-11-30 08:34:11 -0500

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
 def main():
   while True:
-    print("Welcome d
+    print("Welcome to the next episode of the biggest hit tracklist show.")
+    MainMenu = input('***Press:***\n'
+                    '1 - Create a new playlist\n'
+                    '2 - Add a track at the end of the playlist\n'
+                    '3 - Play the next track\n'
+                    '4 - Play the previous track\n'
+                    '5 - Repeat the current track\n'
+                    '6 - Remove a track from the playlist\n'
+                    '7 - Display the playlist\n'
+                    '8 - Shuffle the playlist\n'
+                    '0 - Quit\n')
+    if MainMenu =='1':
+      
+      
+    elif MainMenu == '2':
+        
+        
+    elif MainMenu == '3':
+          
+          
+    elif MainMenu == '4':
+            
+            
+    elif MainMenu == '5':
+              
+              
+    elif MainMenu == '6':
+                
+                
+    elif MainMenu == '7':
+                  
+                  
+    elif MainMenu == '8':
+                    
+                    
+    elif MainMenu == '0':
+      
+    else:
+      print('Invalid input.')
+      
+      
+if__name__=='__main__':
+  main()
