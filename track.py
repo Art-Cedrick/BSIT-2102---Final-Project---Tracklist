29901b9c6502d1d197eb3c359663011017f5b8a1
21-02497 <113772219+21-02497@users.noreply.github.com>
2022-12-06 23:47:16 +0800

    Update track.py

--- track.py
+++ track.py
@@ -1,18 +1,30 @@
 class Track:
   
   def __init__(self, title, artist):
+    """
+    Initializes the song.
+    
+    """
     self.__title = title
     self.__artist = artist
     
   def get_title(self):
+    """
+    Returns the title of the song.
+    
+    """
     return self.__title
   
   def get_artist(self):
+    """
+    Returns the artist of the song.
+    
+    """
     return self.__artist
   
   def __str__(self):
+    """
+    Returns the string representation of the song.
+    
+    """
     return '\"{}\" by {}\n'.format(self.__title, self.__artist)
-  
-  def __eq__(self, other):
-    return self.__title == other.get_title() \
-           and self.__artist == other.get_artist()
