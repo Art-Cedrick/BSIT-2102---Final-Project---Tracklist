b3f59f439c2ae69c820e1b2c0140ef1bfb8e1700
Kim William Sanchez <114321964+21-05298@users.noreply.github.com>
2022-12-06 19:14:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,77 @@
-from playlist import Playlist
+from tracklist import Tracklist
 from track import Track
 
 def main():
   while True:
     print("Welcome to the next episode of the biggest hit tracklist show.")
     MainMenu = input('***Press:***\n'
-                    '1 - Create a new playlist\n'
-                    '2 - Add a track at the end of the playlist\n'
-                    '3 - Play the next track\n'
-                    '4 - Play the previous track\n'
-                    '5 - Repeat the current track\n'
-                    '6 - Remove a track from the playlist\n'
-                    '7 - Display the playlist\n'
-                    '8 - Shuffle the playlist\n'
+                    '1 - Create a new tracklist\n'
+                    '2 - Add a track at the start of the tracklist\n'
+                    '3 - Add a track at the end of the tracklist\n'
+                    '4 - Play the next track\n'
+                    '5 - Play the previous track\n'
+                    '6 - Repeat the current track\n'
+                    '7 - Remove a track from the tracklist\n'
+                    '8 - Display the tracklist\n'
+                    '9 - Shuffle the tracklist\n'
                     '0 - Quit\n')
     if MainMenu =='1':
           UserName = input('Enter your name: ')
-          playlist = Playlist(UserName, 20 ) 
+          tracklist = Tracklist(UserName, 20 ) 
           print(f"Playlist created for {UserName}.")
-          print(playlist)
+          print(tracklist)
+          
           
     elif MainMenu == '2':
+			    TrackTitle = input('Enter the song title: ')
+			    TrackArtist = input('Enter the song artist: ')
+			    track = Track(TrackTitle, TrackArtist)
+			try:
+				       tracklist.addTracktoFirst (track)
+			except NameError:
+				        print('Please create a tracklist first.')
+    elif MainMenu == '3':
           TrackTitle = input('Enter the track title: ')
           TrackArtist = input('Enter the track artist: ')
           track = Track(TrackTitle, TrackArtist)
         try: 
-               playlist.addTrack(track)
+               tracklist.addTracktoLast(track)
         except NameError:
-               print('Please create a playlist first.')
-            
-    elif MainMenu == '3':
-        try: 
-            playlist.playNext()
-        except NameError:
-              print('Please create a playlist first.')
+               print('Please create a tracklist first.')
             
     elif MainMenu == '4':
-        try:
-            playlist.playPrevious()
+        try: 
+            tracklist.playNext()
         except NameError:
-              print('Please create a playlist first.')
+              print('Please create a tracklist first.')
             
     elif MainMenu == '5':
-        try: 
-             playlist.repeat()
+        try:
+            tracklist.playPrevious()
         except NameError:
-              print('Please create a playlist first.')
+              print('Please create a tracklist first.')
             
     elif MainMenu == '6':
+        try: 
+             tracklist.repeat()
+        except NameError:
+              print('Please create a tracklist first.')
+            
+    elif MainMenu == '7':
         trackToRemove = input('Enter the track to remove: ')
         try:
-            playlist.removeTrack(trackToRemove)
+            tracklist.removeTrack(trackToRemove)
         except NameError:
-            print('Please create a playlist first.')
+            print('Please create a tracklist first.')
                 
-    elif MainMenu == '7':
-        print(playlist)
-                  
     elif MainMenu == '8':
+        print(tracklist)
+                  
+    elif MainMenu == '9':
         try:
-            playlist.shuffle()
+            tracklist.shuffle()
         except NameError:
-            print('Please create a playlist first.')
+            print('Please create a tracklist first.')
                     
     elif MainMenu == '0':
         break
